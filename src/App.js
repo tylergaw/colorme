9b3384ae729f42ecbfdaf3f44313db011b0064dc
Tyler Gaw <me@tylergaw.com>
2016-12-31 01:30:10 -0500

    Adds url param base color setting. Fixes more stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import './App.css';
-
 import React, { Component } from 'react';
-import {clone, findIndex, propEq} from 'ramda';
+import {findIndex, propEq} from 'ramda';
 
 import color from 'color';
 import colorFn from 'css-color-function';
-import colorString from 'color-string';
 
-const availableAdjusters = [
+const DEFAULT_BASE_COLOR = '#2ac20d';
+
+const ADJUSTERS = [
   {
     enabled: false,
     name: 'alpha',
     shortName: 'a',
+    unit: '%'
+  },
+  {
+    enabled: false,
+    name: 'saturation',
     unit: '%',
-    value: 100
+    shortName: 's'
   },
   {
     enabled: false,
     name: 'hue',
-    shortName: 'h',
-    value: null
+    max: 360,
+    shortName: 'h'
+  },
+  {
+    enabled: false,
+    name: 'lightness',
+    unit: '%',
+    shortName: 'l'
+  },
+  {
+    enabled: false,
+    name: 'whiteness',
+    unit: '%',
+    shortName: 'w'
+  },
+  {
+    enabled: false,
+    name: 'blackness',
+    unit: '%',
+    shortName: 'b'
   },
   {
     enabled: false,
@@ -35,74 +57,156 @@ const availableAdjusters = [
   },
   {
     enabled: false,
-    name: 'saturation',
-    shortName: 's',
-    value: 0
-  },
-  {
-    enabled: false,
-    defaultValue: 0,
     name: 'contrast',
     unit: '%',
     value: 0
   }
 ];
 
-const getColorReport = (inputColor) => {
-  // Don't return a report if this is an invalid color.
-  if (!colorString.get(inputColor)) {
-    return null
-  };
-
-  const outputColorObj = color(inputColor);
+/**
+ * @param {String} inputColor - A valid hex, rgb, rgba color.
+ * @return {Object} An object containing the properties of the given inputColor.
+ *                  properties include; rgb, hsl, hwb, alpha, lightness,
+ *                  blackness, whiteness.
+ */
+const getColorProperties = (inputColor) => {
+  const colorObj = color(inputColor);
+  const {valpha} = colorObj;
+  const {
+    r: red,
+    g: green,
+    b: blue
+  } = colorObj.object();
 
   const {
-    valpha: alpha,
-    color: [h, s, l]
-  } = outputColorObj.hsl();
+    h: hue,
+    s: saturation,
+    l: lightness
+  } = colorObj.hsl().object();
 
-  return {
-    alpha: alpha * 100,
-    hue: Math.ceil(h),
-    saturation: Math.ceil(s),
-    luminance: Math.ceil(l)
+  const {
+    b: blackness,
+    w: whiteness
+  } = colorObj.hwb().object();
+
+  const properties = {
+    alpha: valpha * 100,
+    hue,
+    lightness,
+    saturation,
+    blackness,
+    whiteness,
+    red,
+    green,
+    blue
   };
+
+  // Round any floats up on the way out.
+  return Object.keys(properties).reduce((prev, curr) => {
+    prev[curr] = Math.ceil(properties[curr]);
+    return prev;
+  }, {});
+};
+
+/**
+ * @param {String} inputColor - A valid hex, rgb, rgba color.
+ * @return {Array} An array of adjuster objects customized to fit the inputColor.
+ */
+const getColorAdjusters = (inputColor) => {
+  const colorProperties = getColorProperties(inputColor);
+
+  return ADJUSTERS.reduce((prev, curr) => {
+    const adjuster = {...curr};
+    const prop = colorProperties[curr.name];
+
+    if (prop !== undefined) {
+      adjuster.value = prop;
+    }
+
+    prev.push(adjuster);
+    return prev;
+  }, []);
+};
+
+/**
+ * @param {String} val - A string that looks like a hex, rgb, or rgba color.
+ * @return {String} A valid color as a string.
+ */
+const getColorFromQueryVal = (val) => {
+  const {
+    hash,
+    search
+  } = window.location;
+
+  let baseColor = null;
+  const decoded = decodeURIComponent(val);
+
+  // Assume hex value with no "#" if we're at that length.
+  if (val.length === 3 || val.length === 6) {
+    baseColor = decoded.match('#') ? decoded : `#${val}`;
+  // Assume the user put an non-encoded "#" in the value for a hex color.
+  } else if (val.length === 0) {
+    if (hash) {
+      baseColor = hash;
+    }
+  } else {
+    baseColor = decodeURIComponent(val);
+  }
+
+  // One last check. If the baseColor provided is invalid, the call to
+  // color() here will throw an error and we fall back to default base.
+  try {
+    color(baseColor);
+  } catch (err) {
+    baseColor = null;
+    console.warn(`Invalid color provided in URL: ${search}, using default base color.`);
+  }
+
+  return baseColor;
 };
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    adjusters: getColorAdjusters(DEFAULT_BASE_COLOR),
+    adjustersStr: '',
+    colorFuncStr: '',
+    inputColor: DEFAULT_BASE_COLOR,
+    inputColorDisplay: DEFAULT_BASE_COLOR,
+    outputColor: DEFAULT_BASE_COLOR
+  };
 
-    const defaultInputColor = '#5f70ee';
-    const report = getColorReport(defaultInputColor);
-    const adjusters = availableAdjusters.reduce((prev, curr) => {
-      const clone = {
-        ...curr
-      };
+  componentWillMount() {
+    const {
+      search
+    } = window.location;
 
-      const inputColorValue = report[curr.name];
+    if (search.indexOf('color') > -1) {
+      const [,queryVal] = search.replace('?', '').split('=');
+      const baseColor = getColorFromQueryVal(queryVal) || DEFAULT_BASE_COLOR;
 
-      if (inputColorValue !== undefined) {
-        clone.value = inputColorValue;
-      }
-
-      prev.push(clone);
-      return prev;
-    }, []);
-
-    this.state = {
-      adjusters,
-      adjustersStr: '',
-      colorFuncStr: '',
-      inputColor: defaultInputColor,
-      outputColor: defaultInputColor
-    };
+      this.setState({
+        adjusters: getColorAdjusters(baseColor),
+        inputColor: baseColor,
+        inputColorDisplay: baseColor,
+        outputColor: baseColor
+      })
+    }
   }
 
   inputColorOnChange = (event) => {
-    this.setState({
-      inputColor: event.target.value
-    });
+    const inputValue = event.target.value;
+    try {
+      color(inputValue);
+
+      this.setState({
+        inputColor: inputValue,
+        inputColorDisplay: inputValue
+      });
+    } catch(err) {
+      this.setState({
+        inputColorDisplay: inputValue
+      });
+    };
   }
 
   adjusterOnChange = (event) => {
@@ -113,7 +217,7 @@ class App extends Component {
 
     const isToggle = event.target.type === 'checkbox';
     const adjusterName = event.target.name.replace('Value', '');
-    const nextAdjusters = clone(adjusters);
+    const nextAdjusters = [...adjusters];
     const index = findIndex(propEq('name', adjusterName))(nextAdjusters);
 
     if (isToggle) {
@@ -137,7 +241,7 @@ class App extends Component {
     }, '');
 
     const colorFuncStr = (adjustersStr.length) ?
-      `color(${inputColor} ${adjustersStr})` : '';
+      `color(${inputColor}${adjustersStr})` : '';
 
     const converted = colorFn.convert(colorFuncStr) || inputColor;
 
@@ -154,15 +258,15 @@ class App extends Component {
       adjusters,
       colorFuncStr,
       inputColor,
+      inputColorDisplay,
       outputColor
     } = this.state;
-
-    console.log(outputColor);
 
     const adjusterOptions = adjusters.map(a => {
       const {
         enabled,
         name,
+        max = 100,
         value
       } = a;
 
@@ -177,7 +281,7 @@ class App extends Component {
               <label>
                 Value
               </label>
-              <input type='number' name={`${name}Value`} min='0' max='100'
+              <input type='number' name={`${name}Value`} min='0' max={max}
                 defaultValue={value} onChange={this.adjusterOnChange} />
             </div>
           ) : ''}
@@ -188,19 +292,25 @@ class App extends Component {
     return (
       <div className="app">
 
-        <div style={{backgroundColor: inputColor}}>
+        <div className='colorContainer' style={{backgroundColor: inputColor}}>
           <h1>ColorMe</h1>
           <p>
-            Visualize CSS color functions. Brought to you by Tyler Gaw. Inspired by SassMe
+            Visualize CSS color functions. Brought to you by Tyler Gaw.
+            Inspired by SassMe
           </p>
 
           <label>Base color:</label>
-          <input type='text' value={inputColor} onChange={this.inputColorOnChange} />
+          <input type='text' value={inputColorDisplay}
+            onChange={this.inputColorOnChange} />
         </div>
 
-        <div style={{backgroundColor: outputColor}}>
+        <div className='colorContainer' style={{backgroundColor: outputColor}}>
           <label>Color function</label>
           <input type='text' readOnly value={colorFuncStr} />
+
+          <p>
+            compiled color: <code>{outputColor}</code>
+          </p>
         </div>
 
         <ul>
