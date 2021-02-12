7913a8cf7d9e9ff1a563680166608f2ce8ff6ea4
Tyler Gaw <me@tylergaw.com>
2021-02-12 18:24:34 -0500

    Updates app accent color

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,23 +1,42 @@
-import './Banner.css';
+import "./Banner.css";
 
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Banner extends Component {
   render() {
     return (
-      <header role='banner'>
-        <h1 className='bannerTitle'>
-          ColorMe
-        </h1>
-        <p className='bannerIntro'>
-          Visualize The CSS Color Function<b>*</b>.
-          {' '}Made by <a href='https://tylergaw.com/articles/introducing-colorme' target='_blank' rel='noopener'>Tyler Gaw</a>.
-          {' '}Code on <a href='https://github.com/tylergaw/colorme' target='_blank' rel='noopener'>GitHub</a>.
-          {' '}Inspired by <a href='http://jim-nielsen.com/sassme/' target='_blank' rel='noopener'>SassMe</a>.
+      <header role="banner">
+        <h1 className="bannerTitle">ColorMe</h1>
+        <p className="bannerIntro">
+          Visualize The CSS Color Function<b>*</b>. Made by{" "}
+          <a
+            href="https://tylergaw.com/articles/introducing-colorme"
+            target="_blank"
+            rel="noopener"
+          >
+            Tyler Gaw
+          </a>
+          . Code on{" "}
+          <a
+            href="https://github.com/tylergaw/colorme"
+            target="_blank"
+            rel="noopener"
+          >
+            GitHub
+          </a>
+          . Inspired by{" "}
+          <a
+            href="https://jim-nielsen.com/sassme/"
+            target="_blank"
+            rel="noopener"
+          >
+            SassMe
+          </a>
+          .
         </p>
       </header>
     );
-  };
+  }
 }
 
 export default Banner;
